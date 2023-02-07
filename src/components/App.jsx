07db1bfff82a1b648e0e6ca59ec86d0cb21ee582
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-02-07 21:31:36 +0600

    addtransaction added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import AddTransaction from "./AddTransaction";
 import "./App.css";
 import Balance from "./Balance";
 import Header from "./Header";
@@ -13,6 +14,7 @@ function App() {
         <Balance />
         <IncomeExpense />
         <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   );
