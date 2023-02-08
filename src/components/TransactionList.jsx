e1d42a3fe06e0d5beff44d794ab5e8cf03157836
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-02-08 23:02:50 +0600

    transaction added to transaction list

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
   return (
     <>
       <h3>History</h3>
