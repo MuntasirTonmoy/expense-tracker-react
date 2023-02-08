e397231d6a6b9a117824582601d2c9430bbeb71b
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-02-08 23:15:34 +0600

    transactin dynamic class added

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Transaction = ({ transaction }) => {
   return (
-    <li key={transaction.id} className="minus">
+    <li
+      key={transaction.id}
+      className={transaction.amount > 0 ? "plus" : "minus"}
+    >
       {transaction.text} <span>${transaction.amount}</span>
       <button className="delete-btn">x</button>
     </li>
