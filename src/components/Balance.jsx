5602f990548c628d66d64266e1e54b0bafd2cfc5
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-02-08 23:21:40 +0600

    total balance updated

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -6,10 +6,11 @@ const Balance = () => {
 
   const amounts = transactions.map(transaction => transaction.amount);
 
+  const total = amounts.reduce((acc, item) => (acc += item).toFixed(2));
   return (
     <>
       <h4>Your Balance</h4>
-      <h1>$.00</h1>
+      <h1>${total}</h1>
     </>
   );
 };
